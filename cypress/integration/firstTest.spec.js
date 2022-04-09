d02b27101ca1daab8bc5d57d9bc81400859f7f1d
Weronika <weronika.rudowicz@gmail.com>
2022-04-09 17:34:45 +0200

    3-14_radiobuttons checkboxes

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -75,7 +75,6 @@ describe('Our first suite', () => {
         })
     })
 
-
     it('invoke commands', () => {
         cy.visit('/')
         cy.contains('Forms').click()
@@ -100,7 +99,7 @@ describe('Our first suite', () => {
             })
     })
 
-    it.only('assert property', () => {
+    it('assert property', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
@@ -112,5 +111,40 @@ describe('Our first suite', () => {
                 cy.get('nb-calendar-day-picker').contains('17').click()
                 cy.wrap(input).invoke('prop', 'value').should('contain', 'Apr 17, 2022')
             })
+    })
+
+    it('radiobutton', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+        cy.contains('nb-card', 'Using the Grid')
+            .find('[type="radio"]')
+            .then(radioButtons => {
+                cy.wrap(radioButtons)
+                    .first()
+                    .check({ force: true })
+                    .should('be.checked')
+
+                cy.wrap(radioButtons)
+                    .eq(1)
+                    .check({ force: true })
+
+                cy.wrap(radioButtons)
+                    .eq(0)
+                    .should('not.be.checked')
+
+                cy.wrap(radioButtons)
+                    .eq(2)
+                    .should('be.disabled')
+            })
+    })
+
+    it.only('checkboxes', () => {
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        cy.get('[type="checkbox"]').eq(0).click({ force: true })
+        cy.get('[type="checkbox"]').eq(1).check({ force: true })
     })
 })
