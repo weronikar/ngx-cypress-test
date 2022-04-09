6d32f782367d7b92bbdaef4a794e23040355b90b
Weronika <weronika.rudowicz@gmail.com>
2022-04-09 15:51:33 +0200

    3-11 find, contains, parents

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -4,7 +4,7 @@ describe('Our first suite', () => {
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
-        
+
         //by Tag name
         cy.get('input')
         //by id #
@@ -28,5 +28,26 @@ describe('Our first suite', () => {
 
         //The most recommended way:
         cy.get('[data-cy="imputEmail1"]')
+    })
+
+    it.only('Second test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]', 'Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            .find('button')
+            .should('contain', 'Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+        cy.contains('nb-card', 'Horizontal form')
+            .find('[type="email"]')
     })
 })
