a3c8a0df4256f8b58fc6399c00c80ba9cc387667
Weronika <weronika.rudowicz@gmail.com>
2022-04-09 23:05:35 +0200

    4-21 E2E tests with page Objects

--- cypress/integration/testWithPageObjects.js
+++ cypress/integration/testWithPageObjects.js
@@ -1,15 +1,32 @@
+const { onDatePickerPage } = require("../support/page_objects/datepickerPage")
+const { onFormLayoutsPage } = require("../support/page_objects/formLayoutsPage")
 const { navigateTo } = require("../support/page_objects/navigationPage")
+const { onSmartTablePage } = require("../support/page_objects/smartTablePage")
 
 describe('Test with Page Objects', () => {
     beforeEach('Open application ', () => {
         cy.visit('/')
     })
 
-    it.only('verify navigations across the pages', () => {
+    it('verify navigations across the pages', () => {
         navigateTo.formLayoutsPage()
         navigateTo.datepickerPage()
         navigateTo.toastrPage()
         navigateTo.smartTablePage()
         navigateTo.tooltipPage()
+    })
+
+    it.only('Should submit Inline and Basic form and select tomorow date in the calendar', () => {
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail('Weronika', 'weronika@test.com')
+        onFormLayoutsPage.submitBasicFormWithEmailAndPassword('weronika@test.com', 'password')
+        navigateTo.datepickerPage()
+        onDatePickerPage.selectCommonDatepickerDateFromToday(1)
+        onDatePickerPage.selectDatepickerWithRangeFromToday(7, 14)
+
+        navigateTo.smartTablePage()
+        onSmartTablePage.addNewRecordWithFirstNameAndLastName('Weronika', 'Marechal')
+        onSmartTablePage.updateAgeByFirstName('Weronika', '33')
+        onSmartTablePage.deleteRowByIndex(1)
     })
 })
